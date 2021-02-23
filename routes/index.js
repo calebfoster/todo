fd2d24bbd64e8d09f2bd1cbdc5f56721defe144c
Caleb Foster <foster.caleb@gmail.com>
2021-02-23 04:29:18 -0600

    react build to public dir

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Todo App' });
+  // res.render('index', { title: 'Todo App' });
+  res.sendFile(path.join(__dirname, '../public', 'app.html'))
 });
 
 module.exports = router;
